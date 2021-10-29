fb0ea5f8874a816682f0c4d662aa92cc37f98d10
Pierre-Aurelien <p.gilliot@instadeep.com>
2021-10-29 12:05:29 +0100

    complte ci

--- forecast/figure/ci_coverage.py
+++ forecast/figure/ci_coverage.py
@@ -30,7 +30,10 @@ def parse_args():
     )
     parser.add_argument("--bins", type=int, default=12, help="Number of bins.")
     parser.add_argument(
-        "--size", type=float, default=1e6, help="Number of bacteria sorted trough the FACS."
+        "--ci_level",
+        type=int,
+        default=3,
+        help="Number of standard deviation to create the confidence interval.",
     )
     parser.add_argument(
         "--reads", type=float, default=1e7, help="Number of reads allocated to sequencing."
@@ -54,26 +57,27 @@ def parse_args():
     parser.add_argument(
         "--fluorescence_amplification",
         type=int,
-        default=100,
+        default=10,
         help="Ratio fluorescence/protein.",
     )
     args = parser.parse_args()
     return args
 
 
-def ci_coverage(a, b, c):
-    """Check if estimate is in 2*confidence interval.
+def ci_coverage(a, b, c, d):
+    """Check if estimate is in 3*confidence interval.
 
     Args:
         a: estimate
         b: ground truth
         c: confidence interval length
+        d: number of confidence interval
 
     Returns:
         1 (inside) or 0 (outside)
     """
     t = 0
-    if a < b - 2 * c or a > b + 2 * c:
+    if a < b - d * c or a > b + d * c:
         t = 0
     else:
         t = 1
@@ -88,26 +92,29 @@ def main():
 
     output_path.mkdir(parents=True, exist_ok=True)
 
-    df_truth = pd.read_csv(Path(args.metadata_path) / "library_gamma.csv")
     df_estimated = pd.read_csv(Path(args.metadata_path) / "results.csv")
+    filter_length = len(df_estimated)
+    df_truth = pd.read_csv(Path(args.metadata_path) / "library_gamma.csv")[:filter_length]
+    df_truth["B_Protein"] = args.fluorescence_amplification * df_truth["B_Protein"]
 
     df = pd.concat([df_truth, df_estimated], axis=1)
     df["a_in_CI"] = df.apply(
-        lambda row: ci_coverage(row["a_MLE"], row["A_Protein"], row["a_std"]), axis=1
+        lambda row: ci_coverage(row["a_MLE"], row["A_Protein"], row["a_std"], args.ci_level), axis=1
     )
+    coverage_a = df["a_in_CI"].mean()
     df["b_in_CI"] = df.apply(
-        lambda row: ci_coverage(row["b_MLE"], row["B_Protein"], row["b_std"]), axis=1
+        lambda row: ci_coverage(row["b_MLE"], row["B_Protein"], row["b_std"], args.ci_level), axis=1
     )
-
+    coverage_b = df["b_in_CI"].mean()
     sns.despine()
 
     fig_dims = (20, 8)
-    x = np.linspace(0, 2, 50)
+    x = np.linspace(0, 2, filter_length)
     _, ax = plt.subplots(figsize=fig_dims)
     ax.errorbar(
         x,
-        (df.sort_values("A_Protein", ascending=False)["a_MLE"])[160:210],
-        yerr=2 * (df.sort_values("A_Protein", ascending=False)["a_std"])[160:210],
+        (df.sort_values("A_Protein", ascending=False)["a_MLE"]),
+        yerr=3 * (df.sort_values("A_Protein", ascending=False)["a_std"]),
         fmt=" ",
         color="tab:brown",
         marker="o",
@@ -118,43 +125,70 @@ def main():
         label="ML estimator",
         zorder=1,
     )
-    labl = df.sort_values("A_Protein", ascending=False)["a_in_CI"][160:210].to_numpy()
+    labl = df.sort_values("A_Protein", ascending=False)["a_in_CI"].to_numpy()
     color = ["tab:orange" if item == 1 else "tab:red" for item in labl]
     ax.scatter(
         x,
-        (df.sort_values("A_Protein", ascending=False)["A_Protein"])[160:210],
+        (df.sort_values("A_Protein", ascending=False)["A_Protein"]),
         s=20,
         label="Ground truth",
         color=color,
         zorder=10,
     )
-    # ax.scatter(x,(df.sort_values('A_Protein',ascending=False)['a_MOM'])[160:210],s=50,label='MOM estimator',c='#4f7942',zorder=13)
+    # ax.scatter(x,(df.sort_values('A_Protein',ascending=False)['a_MOM']),s=50,label='MOM estimator',c='#4f7942',zorder=13)
     ax.legend(frameon=False, fontsize=16, markerscale=1.5)
+
     ax.axes.get_xaxis().set_visible(False)
     plt.xlabel("Construct Number")
     plt.ylabel("Estimated mean protein copy number", fontsize=16)
-    plt.title("Confidence Intervals MLE")
+    plt.title(f"Confidence Intervals MLE. Coverage is {coverage_a}.")
     plt.savefig(
         Path(output_path) / "a_confidence_interval.png",
         transparent=True,
         bbox_inches="tight",
         dpi=600,
     )
+    plt.close()
 
-    # fig_dims = (20, 8)
-    # x = np.linspace(0, 2, 50)
-    # fig, ax = plt.subplots(figsize=fig_dims)
-    # ax.errorbar(x,(df.sort_values('B_Protein',ascending=False)['b_MLE'])[160:210], yerr=2*(df.sort_values('B_Protein',ascending=False)['a_std'])[160:210],  fmt=' ',color='tab:brown', marker='o', markersize=7,
-    #              ecolor='gray', elinewidth=2.5, capsize=4,label='ML estimator',zorder=1)
-    # labl=df.sort_values('B_Protein',ascending=False)['b_in_CI'][160:210].to_numpy()
-    # color= ['tab:orange' if l == 1 else 'tab:red' for l in labl]
-    # ax.scatter(x,(df.sort_values('B_Protein',ascending=False)['B_Protein'])[160:210],s=20,label='Ground truth',color=color,zorder=10)
-    # # ax.scatter(x,(df.sort_values('B_Protein',ascending=False)['b_MOM'])[160:210],s=50,label='MOM estimator',c='#4f7942',zorder=13)
-    # ax.legend(frameon=False,fontsize=16,markerscale=1.5)
-    # ax.axes.get_xaxis().set_visible(False)
-    # plt.xlabel('Construct Number')
-    # plt.ylabel('Estimated std protein copy number',fontsize=16)
-    # plt.title('Confidence Intervals MLE')
+    fig_dims = (20, 8)
+    _, ax = plt.subplots(figsize=fig_dims)
+    ax.errorbar(
+        x,
+        (df.sort_values("B_Protein", ascending=False)["b_MLE"]),
+        yerr=2 * (df.sort_values("B_Protein", ascending=False)["a_std"]),
+        fmt=" ",
+        color="tab:brown",
+        marker="o",
+        markersize=7,
+        ecolor="gray",
+        elinewidth=2.5,
+        capsize=4,
+        label="ML estimator",
+        zorder=1,
+    )
+    labl = df.sort_values("B_Protein", ascending=False)["b_in_CI"].to_numpy()
+    color = ["tab:orange" if item == 1 else "tab:red" for item in labl]
+    ax.scatter(
+        x,
+        (df.sort_values("B_Protein", ascending=False)["B_Protein"]),
+        s=20,
+        label="Ground truth",
+        color=color,
+        zorder=10,
+    )
+    # ax.scatter(x,(df.sort_values('B_Protein',ascending=False)['b_MOM']),s=50,label='MOM estimator',c='#4f7942',zorder=13)
+    ax.legend(frameon=False, fontsize=16, markerscale=1.5)
+    ax.axes.get_xaxis().set_visible(False)
+    plt.xlabel("Construct Number")
+    plt.ylabel("Estimated std protein copy number", fontsize=16)
+    plt.title(f"Confidence Intervals MLE. Coverage is {coverage_b} ")
+    plt.savefig(
+        Path(output_path) / "b_confidence_interval.png",
+        transparent=True,
+        bbox_inches="tight",
+        dpi=600,
+    )
+    plt.close()
 
 
 if __name__ == "__main__":
